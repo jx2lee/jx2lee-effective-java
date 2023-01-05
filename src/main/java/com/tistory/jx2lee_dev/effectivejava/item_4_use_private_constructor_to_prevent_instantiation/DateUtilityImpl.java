f6175d3d022a546c7b747a55401cb0d939fe101b
jx2lee <jaejun.lee.1991@gmail.com>
2023-01-06 00:16:57 +0900

    use private constructor to prvent instantiation

---
+++ src/main/java/com/tistory/jx2lee_dev/effectivejava/item_4_use_private_constructor_to_prevent_instantiation/DateUtilityImpl.java
@@ -0,0 +1,4 @@
+package com.tistory.jx2lee_dev.effectivejava.item_4_use_private_constructor_to_prevent_instantiation;
+
+public class DateUtilityImpl extends AbstractDateUtility {
+}
---
+++ src/main/java/com/tistory/jx2lee_dev/effectivejava/item_4_use_private_constructor_to_prevent_instantiation/DateUtilityWithPrivateConstructor.java
@@ -0,0 +1,16 @@
+package com.tistory.jx2lee_dev.effectivejava.item_4_use_private_constructor_to_prevent_instantiation;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtilityWithPrivateConstructor {
+    private DateUtilityWithPrivateConstructor() {
+        throw new AssertionError();
+    }
+
+    private static String FULL_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
+
+    public static String convertDateToString(Date date) {
+        return new SimpleDateFormat(FULL_DATE_FORMAT).format(date);
+    }
+}
